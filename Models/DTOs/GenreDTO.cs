38895d1ed4512e0e052a2c984e4e19f84a1f01a0
MK396 <272552@student.pwr.edu.pl>
2025-01-06 22:46:33 +0100

    zmiany

---
+++ Models/DTOs/GenreDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookShop.Models.DTOs
+{
+    /// <summary>
+    /// Przechowuje i przekazuje dane o gatunku książki
+    /// </summary>
+    public class GenreDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string GenreName { get; set; }
+
+    }
+}
