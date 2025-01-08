1b6bc8b553bcfa6de3109ed8296ed0ec1926e83b
MK396 <147173560+MK396@users.noreply.github.com>
2025-01-08 19:21:24 +0100

    Delete Documentation directory

--- Documentation/namespace_book_shop_1_1_data.js
+++
@@ -1,5 +0,0 @@
-var namespace_book_shop_1_1_data =
-[
-    [ "ApplicationDbContext", "class_book_shop_1_1_data_1_1_application_db_context.html", null ],
-    [ "DbSeeder", "class_book_shop_1_1_data_1_1_db_seeder.html", null ]
-];
--- Documentation/namespaces_dup.js
+++
@@ -1,4 +0,0 @@
-var namespaces_dup =
-[
-    [ "BookShop", "namespace_book_shop.html", "namespace_book_shop" ]
-];
